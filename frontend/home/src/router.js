01782f5227211a9ade369e6c741d508c4575507b
baekkyunshin <werooring@gmail.com>
2018-12-21 21:12:45 +0900

    [#7] trailing slash, import 방식  수정
    - App.vue에 trailing slash 추가
    
    - router.js에서 blog component import 방법 변경

--- frontend/home/src/router.js
+++ frontend/home/src/router.js
@@ -1,11 +1,15 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Meta from 'vue-meta'
+import Vue from "vue";
+import Router from "vue-router";
+import Meta from "vue-meta";
 
-import Home from './components/Home.vue'
-import About from './components/About.vue'
-import BookThinkBlog from './components/BookThinkBlog.vue'
-import DevRecordBlog from './components/DevRecordBlog.vue'
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+// import BookThinkBlog from "./components/BookThinkBlog.vue";
+// import DevRecordBlog from "./components/DevRecordBlog.vue";
+
+const BookThinkBlog = () => import("./components/BookThinkBlog.vue");
+const DevRecordBlog = () => import("./components/DevRecordBlog.vue");
+
 
 // Asynchronously import components
 // Reasons for asynchoronous load:
