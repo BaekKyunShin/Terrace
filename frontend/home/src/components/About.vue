37ef6daa17e890636c96ee15d1c445b85d43ce4f
baekkyunshin <werooring@gmail.com>
2018-12-23 18:21:52 +0900

    [#12] 프로필 사진에 그림자 효과 적용
    Resolves #12

--- frontend/home/src/components/About.vue
+++ frontend/home/src/components/About.vue
@@ -31,6 +31,7 @@ body {
   border-radius: 15px;
   width: 150px;
   height: 200px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 #personalInfo {
   font-family: Courier New;
