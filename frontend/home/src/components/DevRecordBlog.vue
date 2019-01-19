7f0e56d8c67e00e7976fdab8e9ee8aa924daec87
baekkyunshin <werooring@gmail.com>
2019-01-19 18:59:26 +0900

    [#19] 개발과 기록 블로그에도 동일하게 적용

--- frontend/home/src/components/DevRecordBlog.vue
+++ frontend/home/src/components/DevRecordBlog.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="blog">
-    <div class="blog-contents-element" v-if="year2 !== undefined" v-html="articleHtmlSource">
+    <div id="blog-contents-element" v-if="year !== undefined">
+      <article>
+        <div class="inner-title-container">
+          <h1 id="inner-title">{{ title }}</h1>
+          <div class="meta">{{ year }} / {{ month }} / {{ day }}</div>
+        </div>
+        <div id="article-content" v-html="articleHtmlSource"></div>
+      </article>
+      <hr>
     </div>
     <div v-else>
       <div class="blog-list-element" v-for="i in index" :key="i">
@@ -26,13 +34,55 @@ import _ from 'highlight.js/styles/xcode.css'
 
 export default {
   name: 'devRecordBlog',
+  metaInfo () {
+    if(this.title === undefined) {
+      return {
+        title: 'Blog',
+        meta: [
+          { charset: 'utf-8' },
+          {
+            'property': 'og:title',
+            'content': 'Blog',
+            'template': chunk => `${chunk} :: bkshin`,
+            'vmid': 'og:title'
+          },
+          {
+            'property': 'og:description',
+            'content': 'Article List',
+            'template': chunk => `${chunk}`,
+            'vmid': 'og:description'
+          },
+        ]
+      }
+    } else {
+      return {
+        title: this.titleForMeta,
+        meta: [
+          { charset: 'utf-8' },
+          {
+            'property': 'og:title',
+            'content': this.title,
+            'template': chunk => `${chunk} :: bkshin`,
+            'vmid': 'og:title'
+          },
+          {
+            'property': 'og:description',
+            'content': 'Thank you for reading my article ',
+            'template': chunk => `${chunk}`,
+            'vmid': 'og:description'
+          }
+        ]
+      }
+    }
+  },
+  
   methods: {
     totheTop: function() {
       document.body.scrollTop = 0; // For Safari
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     },
     getPage: function() {
-      if (this.year2 === undefined) {
+      if (this.year === undefined) {
         return;
       }
 
@@ -41,10 +91,10 @@ export default {
 
       var htmlDocUri = 
         '/blog_contents/dev_record/'
-        + this.year2 + '/'
-        + this.month2 + '/'
-        + this.day2 + '/'
-        + this.title2 + '.html';
+        + this.year + '/'
+        + this.month + '/'
+        + this.day + '/'
+        + this.title + '.html';
       fetch(htmlDocUri)
         .then(response => response.text())
         .then(responseText => this.articleHtmlSource = responseText);
@@ -52,13 +102,31 @@ export default {
   }, 
   mounted: function() {
     this.getPage();
-    // Find all code block and apply syntax highlighting
-    [].forEach.call(document.querySelectorAll('code'), function(el) {
-      HighlightJS.highlightBlock(el);
-    });
   },
 
   updated: function() {
+    // Change file name to document's title
+    // When the page is an artice page, get blogContents
+    var blogContents = document.querySelector('#blog-contents-element');
+    if(blogContents == null){
+      //this.toTheTop();
+      return;
+    }
+    // Now, below scripts are for displaying an article,
+    // Not the blog main page which has lists of name of articles.
+    // Find all h1 tags, and choose second h1. It is real title of this doc.
+    var titles = blogContents.querySelectorAll('h1');
+    if(titles.length <= 1) {
+      return;
+    } else {
+      // Title exists
+      var title = blogContents.querySelector('#inner-title')
+      title.innerHTML = titles[1].innerHTML;
+      titles[1].style.display = "none";
+      // It is for og:title
+      this.titleForMeta = title.innerHTML;
+    }
+    
     // Find all code block and apply syntax highlighting
     [].forEach.call(document.querySelectorAll('code'), function(el) {
       HighlightJS.highlightBlock(el);
@@ -71,19 +139,19 @@ export default {
       index: 
 [{"title":"첫 글","author":"myself","uri":"/devRecordBlog/2018/12/12/thanks-to/","date":"2018/12/12"}] // __INSERTION_POSITION_END__ // DONT CHANGE!!
 ,
-    year2 : this.$route.params.year2,
-    month2 : this.$route.params.month2,
-    day2 : this.$route.params.day2,
-    title2 : this.$route.params.title2,
+    year : this.$route.params.year2,
+    month : this.$route.params.month2,
+    day : this.$route.params.day2,
+    title : this.$route.params.title2,
     articleHtmlSource : ""
     }
   },
   watch: {
     '$route' (to) {
-      this.year2 = to.params.year2;
-      this.month2 = to.params.month2;
-      this.day2 = to.params.day2;
-      this.title2 = to.params.title2;
+      this.year = to.params.year2;
+      this.month = to.params.month2;
+      this.day = to.params.day2;
+      this.title = to.params.title2;
       this.getPage();
     }
   }
@@ -131,6 +199,24 @@ export default {
   border: 0;
   height: 1px;
   background: #f2f2f2;
+}
+
+.inner-title-container {
+  text-align: center;
+  text-decoration: none;
+}
+
+.inner-title-container > h1 {
+  margin-bottom: 10px;
+}
+
+#article-content {
+  text-align: left;
+}
+
+.meta {
+  margin-bottom: 50px;
+  font-size: 0.9em;
 }
 
 a {
