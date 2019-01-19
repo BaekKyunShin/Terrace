a9cb3c16ee713e2b6de80c36c2de48fe37284675
baekkyunshin <werooring@gmail.com>
2019-01-19 17:50:17 +0900

    [#19] 블로그 컨텐츠 제목 수정
    - html 상의 제목 숨기기
    
    - html 상의 제목을 블로그 콘텐츠 제목으로 바인딩 시키기
    
    - 날짜 바인딩

--- frontend/home/src/components/BookThinkBlog.vue
+++ frontend/home/src/components/BookThinkBlog.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="blog">
-    <div class="blog-contents-element" v-if="year1 !== undefined" v-html="articleHtmlSource">
+    <div id="blog-contents-element" v-if="year !== undefined">
+      <article>
+        <div class="inner-title-container">
+          <h1 id="inner-title">{{ title  }}</h1>
+          <p class="meta">{{ year }} / {{ month }} / {{ day }}</p>
+        </div>
+        <div id="article-content" v-html="articleHtmlSource"></div>
+      </article>
+      <hr>
     </div>
     <div v-else>
       <div class="blog-list-element" v-for="i in index" :key="i">
@@ -17,28 +25,95 @@
 <script>
 export default {
   name: 'bookThinkBlog',
+  metaInfo() {
+    if(this.year === undefined) {
+      return {
+        title: 'Blog',
+        meta: [
+          { charset: 'utf-8' },
+          {
+            'property': 'og:title',
+            'content': 'Blog',
+            'template': chunk => `${chunk} | bksin`,
+            'vmid': 'og:title'
+          },
+          {
+            'property': 'og:description',
+            'content': 'Article List',
+            'template': chunk => `${chunk}`,
+            'vmid': 'og:description'
+          },
+        ]
+      }
+    } else {
+      return {
+        title: this.titleForMeta,
+        meta: [
+          { charset: 'utf-8' },
+          {
+            'property': 'og:title',
+            'content': this.title,
+            'template': chunk => `${chunk} | bkshn`,
+            'vmid': 'og:title'
+          },
+          {
+            'property': 'og:description',
+            'content': 'Thank you for reading my article',
+            'template': chunk => `${chunk}`,
+            'vmid': 'og:description'
+          }
+        ]
+      }
+    }
+  },
+ 
   mounted: function() {
     this.getPage();
   },
+
+  updated: function() {
+    // Change file name to document's title
+    // When the page is an artice page, get blogContents
+    var blogContents = document.querySelector('#blog-contents-element');
+    if(blogContents == null){
+      //this.toTheTop();
+      return;
+    }
+    // Now, below scripts are for displaying an article,
+    // Not the blog main page which has lists of name of articles.
+    // Find all h1 tags, and choose second h1. It is real title of this doc.
+    var titles = blogContents.querySelectorAll('h1');
+    if(titles.length <= 1) {
+      return;
+    } else {
+      // Title exists
+      var title  = blogContents.querySelector('#inner-title')
+      title.innerHTML = titles[1].innerHTML;
+      titles[1].style.display = "none";
+      // It is for og:title
+      this.titleForMeta = title.innerHTML;
+    }
+  },
+
   data() {
     return {
       // __INSERTION_POSITION__ // DONT CHANGE!!
       index: 
 [{"title":"사피엔스","author":"유발 하라리","uri":"/bookThinkBlog/2018/12/28/book-sapiens/","date":"2018/12/28"},{"title":"내가 누군지도 모른 채 마흔이 되었다","author":"제임스 홀리스","uri":"/bookThinkBlog/2018/11/27/book-theMiddlePassage/","date":"2018/11/27"},{"title":"오래된 미래","author":"헬레나 노르베리 호지","uri":"/bookThinkBlog/2018/10/09/book-ancientFutures/","date":"2018/10/09"},{"title":"아내를 모자로 착각한 남자","author":"올리버 색스","uri":"/bookThinkBlog/2018/09/10/book-hisWifeforaHat/","date":"2018/09/10"},{"title":"세상을 만드는 글자, 코딩","author":"박준석","uri":"/bookThinkBlog/2018/08/26/book-wordCoding/","date":"2018/08/26"},{"title":"상실의 시대","author":"무하카미 하루키","uri":"/bookThinkBlog/2018/08/10/book-norwegianWood/","date":"2018/08/10"},{"title":"자유론","author":"존 스튜어트 밀","uri":"/bookThinkBlog/2018/06/17/book-onLiberty/","date":"2018/06/17"},{"title":"장자","author":"장자","uri":"/bookThinkBlog/2018/05/13/book-zhuangzi/","date":"2018/05/13"},{"title":"페르마의 마지막 정리","author":"사이먼 싱","uri":"/bookThinkBlog/2018/04/05/book-fermat/","date":"2018/04/05"},{"title":"철학이야기","author":"윌 듀랜트","uri":"/bookThinkBlog/2018/03/02/book-philosophyStory/","date":"2018/03/02"},{"title":"참을 수 없는 존재의 가벼움","author":"밀란 쿤데라","uri":"/bookThinkBlog/2018/02/11/book-lightnessOfPresence/","date":"2018/02/11"},{"title":"코스모스","author":"칼세이건","uri":"/bookThinkBlog/2017/12/31/book-cosmos/","date":"2017/12/31"},{"title":"백범일지","author":"백범김구","uri":"/bookThinkBlog/2017/07/30/book-kimkoo/","date":"2017/07/30"},{"title":"지금 알고 있는 걸 그때도 알았더라면","author":"류시화 엮음","uri":"/bookThinkBlog/2017/06/27/book-ryusiwha/","date":"2017/06/27"},{"title":"실존주의는 휴머니즘이다","author":"장 폴 샤르트르","uri":"/bookThinkBlog/2017/05/18/book-existentialism/","date":"2017/05/18"},{"title":"이방인","author":"알베르 까뮈","uri":"/bookThinkBlog/2017/04/03/book-stranger/","date":"2017/04/03"}] // __INSERTION_POSITION_END__ // DONT CHANGE!!
 ,
-    year1 : this.$route.params.year1,
-    month1 : this.$route.params.month1,
-    day1 : this.$route.params.day1,
-    title1 : this.$route.params.title1,
+    year : this.$route.params.year1,
+    month : this.$route.params.month1,
+    day : this.$route.params.day1,
+    title : this.$route.params.title1,
     articleHtmlSource : ""
     }
   },
   watch: {
     '$route' (to) {
-      this.year1 = to.params.year1;
-      this.month1 = to.params.month1;
-      this.day1 = to.params.day1;
-      this.title1 = to.params.title1;
+      this.year = to.params.year1;
+      this.month = to.params.month1;
+      this.day = to.params.day1;
+      this.title = to.params.title1;
       this.getPage();
       }
   },
@@ -48,7 +123,7 @@ export default {
       document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     },
     getPage: function() {
-      if (this.year1 === undefined) {
+      if (this.year === undefined) {
         return;
       }
 
@@ -57,10 +132,10 @@ export default {
       
       var htmlDocUri = 
         '/blog_contents/book_think/'
-        + this.year1 + '/'
-        + this.month1 + '/'
-        + this.day1 + '/'
-        + this.title1 + '.html';
+        + this.year + '/'
+        + this.month + '/'
+        + this.day + '/'
+        + this.title + '.html';
       fetch(htmlDocUri)
         .then(response => response.text())
         .then(responseText => this.articleHtmlSource = responseText);
@@ -74,7 +149,6 @@ export default {
   min-height: 300px;
   margin: auto;
   margin-bottom: 50px;
-  text-align: right;
 }
 
 .blog-list-element {
@@ -115,6 +189,19 @@ export default {
   border: 0;
   height: 1px;
   background: #f2f2f2;
+}
+
+.inner-title-container {
+  text-align: center;
+  text-decoration: none;
+}
+
+.inner-title-container > h1{
+  margin-bottom: 10px;
+}
+
+.meta {
+  margin: 0;
 }
 
 a { 
